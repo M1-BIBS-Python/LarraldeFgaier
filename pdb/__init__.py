a9a2ccb2c120b6d553e560f57201d792a9795de8
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-03-14 16:41:05 +0100

    Properly scaffold Python module

---
+++ pdb/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from .protein import Protein
+from .residual import Residual
+from .chain import Chain
+from .atom import Atom
+
+__author__ = "althonos"
+__author_email__ = "martin.larralde@ens-cachan.fr"
+__version__ = "0.1.0"
+__license__ = "GPLv3"
