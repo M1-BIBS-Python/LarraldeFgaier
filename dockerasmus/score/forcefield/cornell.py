8e4d60ad015d44634a4e2ce4cb1d4d3a99d7ac3a
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-03-26 01:25:03 +0100

    Optimize matrix calculation in Cornell forcefield scoring function

--- dockerasmus/score/forcefield/cornell.py
+++ dockerasmus/score/forcefield/cornell.py
@@ -11,35 +11,35 @@ def score(protein1, protein2):
     """Matrix computation of the VDW term of Cornell's force field
     """
 
-    atoms_1 = list(protein1.iteratoms())
-    atoms_2 = list(protein2.iteratoms())
-
     mx_epsilon = compose(
         lambda e1, e2: numpy.sqrt(e1*e2),
-        numpy.array([a.epsilon for a in atoms_1]),
-        numpy.array([a.epsilon for a in atoms_2]),
+        numpy.array([a.epsilon for a in protein1.iteratoms()]),
+        numpy.array([a.epsilon for a in protein2.iteratoms()]),
     )
 
-    mx_radius = compose(
+    mx_radius_6 = compose(
         lambda r1, r2: r1+r2,
-        numpy.array([a.radius for a in atoms_1]),
-        numpy.array([a.radius for a in atoms_2]),
-    )
+        numpy.array([a.radius for a in protein1.iteratoms()]),
+        numpy.array([a.radius for a in protein2.iteratoms()]),
+    )**6
 
-    mx_A = mx_epsilon * mx_radius**12
-    mx_B = 2 * mx_epsilon * mx_radius**6
+    #mx_A = mx_epsilon * mx_radius_12
+    #mx_B = 2 * mx_epsilon * mx_radius_6
+    mx_B = 2 * mx_epsilon * mx_radius_6
+    mx_A = 0.5 * mx_B * mx_radius_6
 
     mx_distance = distance(
-        numpy.array([a.pos for a in atoms_1]),
-        numpy.array([a.pos for a in atoms_2]),
+        numpy.array([a.pos for a in protein1.iteratoms()]),
+        numpy.array([a.pos for a in protein2.iteratoms()]),
     )
+    mx_distance_6 = mx_distance**6
 
     mx_q = compose(
         lambda q1, q2: q1*q2,
-        numpy.array([a.charge for a in atoms_1]),
-        numpy.array([a.charge for a in atoms_2]),
+        numpy.array([a.charge for a in protein1.iteratoms()]),
+        numpy.array([a.charge for a in protein2.iteratoms()]),
     )
 
     return numpy.sum(numpy.triu(k=1, m=\
-        mx_A/(mx_distance**12)-mx_B/(mx_distance**6))+mx_q/(mx_epsilon*mx_distance)
+        mx_A/(mx_distance_6**2)-mx_B/(mx_distance_6)+mx_q/(80*mx_distance))
     )
