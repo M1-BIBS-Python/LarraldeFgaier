4810bb0c1faea48e8f0bfa082f92fc9736c8af9d
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-04-05 18:31:49 +0200

    Switch to bootswatch-simplex doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -107,10 +107,10 @@ html_theme = 'basicstrap'
 # documentation.
 #
 html_theme_options = {
-    'header_inverse': False,
-    'relbar_inverse': False,
+    'header_inverse': True,
+    'relbar_inverse': True,
     'inner_theme': True,
-    'inner_theme_name': 'bootswatch-cosmo',
+    'inner_theme_name': 'bootswatch-simplex',
     'theme_preview': False,
 }
 
