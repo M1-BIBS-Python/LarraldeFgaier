98fad6444fa59b021a12707be7d4aa90a8b8d410
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-04-05 10:35:35 +0200

    Add tests to LennardJones scoring component

--- dockerasmus/score/components/lennard_jones.py
+++ dockerasmus/score/components/lennard_jones.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import absolute_import
 from __future__ import unicode_literals
+from __future__ import division
 
 from .base import BaseComponent
 from .. import requirements
---
+++ tests/test_score/test_components/test_lennard_jones.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import unittest
+import contexter
+import numpy
+
+from dockerasmus.pdb import Protein, Chain, Residual, Atom
+from dockerasmus.score import ScoringFunction
+from dockerasmus.score.components import LennardJones
+from dockerasmus.utils.matrices import distance
+
+from ...utils import mock
+
+
+class TestLennardJones(unittest.TestCase):
+
+    #   TEST SITUATION: 4 atoms of the same kind
+    #   A & B from prot 1, C & D from prot 2
+    #
+    #
+    #   A(id:1, x=0, y=0) B(id:2, x=1, y=0)
+    #     x===============x
+    #     .     PROT 1    .
+    #     .               .
+    #     .               .
+    #     .     PROT 2    .
+    #     x===============X
+    #   C(id:1, x=0, y=1) D(id:2, x=1, y=1)
+    #
+    #     epsilon =  1  1     radius = .5  .5  (so they sum to 1)
+    #                1  1              .5  .5
+    #
+    #    distance =  1 √2   (since A->D = (1,1), B->C = (1,1)
+    #               √2  1      and A->C = (0,1), B->D = (0,1))
+    #
+    # Since we're only summing for i>j, we only are
+    # computing Van der Waals on B & C
+    #
+    # a =     epsilon * radius¹² = 1
+    # b = 2 * epsilon * radius⁶  = 2
+    # d = √2
+    #
+    # LJ = a/d^12  - b/d^6
+    #    = 1/64 - 2/8
+
+
+
+    def setUp(self):
+        self.eps = numpy.array([[1, 1], [1, 1]])
+        self.vdw_radius = numpy.array([[.5, .5], [.5, .5]])
+        self.distance = distance(numpy.array([[0,0], [0,1]]),
+                                 numpy.array([[1,0], [1,1]]))
+
+    def test_numpy(self):
+        lj = LennardJones(force_backend='numpy')
+        self.assertAlmostEqual(
+            float(lj(self.eps, self.vdw_radius, self.distance)),
+            1/64 - 2/8
+        )
+
+    def test_theano(self):
+        lj = LennardJones(force_backend='theano')
+        self.assertAlmostEqual(
+            float(lj(self.eps, self.vdw_radius, self.distance)),
+            1/64 - 2/8
+        )
+
+    def test_wrapped(self):
+        A = Atom(0, 0, 0, 1)
+        B = Atom(1, 0, 0, 2)
+        C = Atom(0, 1, 0, 3)
+        D = Atom(1, 1, 0, 4)
+
+        prot1 = Protein(chains={'A': Chain('A', residuals={1: Residual(1, atoms={'A':A, 'B':B})})})
+        prot2 = Protein(chains={'A': Chain('A', residuals={1: Residual(1, atoms={'C':C, 'D':D})})})
+
+        with mock.patch('dockerasmus.pdb.Atom.epsilon',
+                        new_callable=mock.PropertyMock,
+                        return_value=1):
+            with mock.patch('dockerasmus.pdb.Atom.radius',
+                            new_callable=mock.PropertyMock,
+                            return_value=.5):
+                score_lj = ScoringFunction(LennardJones)
+                self.assertAlmostEqual(score_lj(prot1, prot2), 1/64 - 2/8)
