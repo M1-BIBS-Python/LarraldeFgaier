b3a956ce92a8246fa47894f8021ba45b9fe1b352
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-04-17 14:36:37 +0200

    Add decorator to supress tf log in tests.utils

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import os
+import functools
 
 CURRDIR = os.path.dirname(os.path.abspath(__file__))
 DATADIR = os.path.join(CURRDIR, "data")
@@ -10,3 +11,11 @@ try:
     from unittest import mock
 except ImportError: # pragma: no cover
     import mock
+
+
+def suppress_tf_log(func):
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        with mock.patch.dict('os.environ', {'TF_CPP_MIN_LOG_LEVEL': "3"}):
+            return func(*args, **kwargs)
+    return new_func
